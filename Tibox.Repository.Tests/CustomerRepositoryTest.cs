73fb0d7fff43121e1287eb0006921548918e87f9
juvega <cevegaju@hotmail.com>
2017-02-26 17:57:19 -0500

    Creacion inicial del repositorio con pruebas de unit test.

---
+++ Tibox.Repository.Tests/CustomerRepositoryTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using Tibox.Repository;
+using Tibox.Models;
+
+namespace Tibox.DataAccess.Tests
+{
+    [TestClass]
+    public class CustomerRepositoryTest
+    {
+        private readonly IRepository _repository;
+        public CustomerRepositoryTest()
+        {
+            _repository = new Repository.Repository();
+        }
+        [TestMethod]
+        public void Get_All_Customers()
+        {
+            var result = _repository.GetAllCustomer();
+            Assert.AreEqual(result.Count() > 0, true);
+        }
+
+        [TestMethod]
+        public void Insert_Customer()
+        {
+            var customer = new Customer
+            {
+                FirstName = "Julio",
+                LastName = "Velarde",
+                City = "Huancavelica",
+                Country = "Peru",
+                Phone = "555-555-555"
+            };
+            var result = _repository.InsertCustomer(customer);
+            Assert.AreEqual(result > 0, true);
+        }
+
+        [TestMethod]
+        public void First_Customer_By_Id()
+        {
+            var customer = _repository.GetCustomerById(1);
+            Assert.AreEqual(customer != null, true);
+
+            Assert.AreEqual(customer.Id, 1);
+            Assert.AreEqual(customer.FirstName, "Maria");
+            Assert.AreEqual(customer.LastName, "Anders");
+        }
+
+        [TestMethod]
+        public void Delete_Customer()
+        {
+            var customer = _repository.GetCustomerById(93);
+            Assert.AreEqual(customer != null, true);
+
+            Assert.AreEqual(_repository.DeleteCustomer(customer), true);
+        }
+
+        [TestMethod]
+        public void Update_Customer()
+        {
+            var customer = _repository.GetCustomerById(1);            
+            Assert.AreEqual(customer != null, true);
+
+            Assert.AreEqual(_repository.UpdateCustomer(customer), true);
+        }
+    }
+}
